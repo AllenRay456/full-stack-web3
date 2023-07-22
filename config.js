a9d43ff3b017ffab6a8fdc0e12f22a1afe9503f1
allenRay <yutianriyue@gmail.com>
2023-07-22 11:31:17 +0800

    切换账号重新部署合约

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 
-  export const contractAddress = "0xC8ce305C95875D30d8A88841B0764D4291829b50"
-  export const ownerAddress = "0x9f17AB14aB5c9c145d5C8FdF4785443e18bEc87A"
+  export const contractAddress = "0xa4f012FC29B85B36b54463a2d9D4Dbf8c01BF5bf"
+  export const ownerAddress = "0xED16989CC66BeF6ADC4Bfb27A6204878dB0aFD7d"
   
